5536ba518eaba046853f338b025fb992852b881f
Lúcás Meier <cronokirby@gmail.com>
2018-09-27 13:09:18 +0200

    adds a sprite to the screen

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,9 +5,9 @@ module Lib
 where
 
 import Control.Monad (unless)
-import SDL (($=))
+import SDL (($=), Rectangle(..))
 import qualified SDL
-import SDL.Vect (V2(..), V4(..))
+import SDL.Vect (Point(..), V2(..), V4(..))
 
 
 -- | The window configuration for the game
@@ -24,21 +24,25 @@ run = do
     window <- SDL.createWindow "Hax" windowConfig
     renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
     SDL.showWindow window
-    mainLoop renderer
+    surface <- SDL.loadBMP "resources/sprites/square.bmp"
+    texture <- SDL.createTextureFromSurface renderer surface
+    mainLoop renderer texture
     SDL.destroyWindow window
     SDL.quit
 
 
 -- | The main loop of the game
-mainLoop :: SDL.Renderer -> IO ()
-mainLoop renderer = do
+mainLoop :: SDL.Renderer -> SDL.Texture -> IO ()
+mainLoop renderer texture = do
     events <- SDL.pollEvents
     let quit = any isEscPressed events
     -- Set the window to black
     SDL.rendererDrawColor renderer $= V4 0 0 0 255
     SDL.clear renderer
+    let dest = Just $ Rectangle (P (V2 300 400)) (V2 30 30)
+    SDL.copy renderer texture Nothing dest
     SDL.present renderer
-    unless quit (mainLoop renderer)
+    unless quit (mainLoop renderer texture)
 
 
 -- | Checks whether or not esc is pressed inside of this event
