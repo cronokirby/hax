a3b789fa53fc793981290c10c4e89011a001906d
Lúcás Meier <cronokirby@gmail.com>
2018-10-17 15:59:04 +0200

    Refactor renderer to use a Reader
    Makes things much cleaner to use a reader over SDL.Renderer,
    also makes adding state for rendering effects much easier.

--- src/Drawing.hs
+++ src/Drawing.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-|
 Description: Contains utility functions for drawing sprites
 -}
 module Drawing
-    ( draw
+    ( Rendering
+    , runRendering
+    , draw
     )
 where
 
 import Control.Monad (forM_)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader (MonadReader, ReaderT, ask, runReaderT)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, justifyRight, pack)
 import Foreign.C.Types (CDouble(..), CInt)
@@ -35,58 +40,76 @@ getDestination (Position pos) (Look width _ _) (SpriteSheet w h texture) =
     in Just . fmap round $ Rectangle (P topLeft) shift
 
 
+
+newtype Rendering a = Rendering 
+    { getRendering :: ReaderT SDL.Renderer IO a
+    }
+    deriving (Functor, Applicative, Monad
+             , MonadReader SDL.Renderer, MonadIO
+             )
+
+runRendering :: SDL.Renderer -> Rendering a -> IO a
+runRendering renderer = (`runReaderT` renderer) . getRendering
+
+
 -- | Renders a Look to a position given data about sprites and a renderer
-renderLook :: Position -> Angle -> Look -> Resources -> SDL.Renderer -> IO ()
-renderLook position (Angle alpha) look resources renderer =
+renderLook :: Position -> Angle -> Look -> Resources -> Rendering ()
+renderLook position (Angle alpha) look resources =
     let index = spriteIndex look
         (sheet, source) = getSprite resources index
         dest = getDestination position look sheet
         toFlip = V2 False False
         angle = CDouble alpha
-    in SDL.copyEx renderer (sheetTexture sheet) source dest angle Nothing toFlip
+    in do
+        renderer <- ask
+        SDL.copyEx renderer (sheetTexture sheet) source dest angle Nothing toFlip
 
 -- | Clears the background
-clearScreen :: SDL.Renderer -> IO ()
-clearScreen renderer = do
+clearScreen :: Rendering ()
+clearScreen = do
+    renderer <- ask
     SDL.clear renderer
     SDL.rendererDrawColor renderer $= V4 0 0 0 255
 
 
 -- | Draws all the sprites, including the background
-draw :: (LevelState, [(Position, Maybe Angle, Look)]) -> Resources -> SDL.Renderer -> IO ()
-draw (hud, toDraw) resources renderer = do
-    clearScreen renderer
+draw :: (LevelState, [(Position, Maybe Angle, Look)]) -> Resources -> Rendering ()
+draw (hud, toDraw) resources = do
+    renderer <- ask
+    clearScreen
     forM_ toDraw $ \(pos, angle, look) ->
-        renderLook pos (fromMaybe (Angle 0) angle) look resources renderer
-    drawHud resources renderer hud
+        renderLook pos (fromMaybe (Angle 0) angle) look resources
+    drawHud resources hud
     SDL.present renderer
     
 -- | Draws the head up display
-drawHud :: Resources -> SDL.Renderer -> LevelState -> IO ()
-drawHud resources renderer GameOver  = 
+drawHud :: Resources  -> LevelState -> Rendering ()
+drawHud resources GameOver  = 
     let dest = Just (Rectangle (P (V2 200 350)) (V2 200 50))
-    in drawText "game over" dest resources renderer
-drawHud resources renderer (InLevel polarity health score) =
+    in drawText "game over" dest resources
+drawHud resources (InLevel polarity health score) =
     let sprite = case polarity of
             Pink -> SpHeartPink
             Blue -> SpHeartBlue
         (sheet, source) = getSprite resources sprite
         dest pos = Just (Rectangle (P pos) (V2 30 30))
-        doDraw pos = SDL.copy renderer (sheetTexture sheet) source (dest pos)
     in do
         forM_ (map (fromIntegral . (\x -> 40 * x - 10)) 
-            [1..health]) $ \x -> doDraw (V2 x 10)
-        drawScore score resources renderer
+            [1..health]) $ \x -> do
+                renderer <- ask
+                SDL.copy renderer (sheetTexture sheet) source (dest (V2 x 10))
+        drawScore score resources
     
-drawScore :: Int -> Resources -> SDL.Renderer -> IO ()
+drawScore :: Int -> Resources -> Rendering ()
 drawScore score = drawText txt dest
  where
     correctedScore = min (max score 0) 999999999
     txt = justifyRight 9 '0' . pack . show $ correctedScore
     dest = Just (Rectangle (P (V2 390 5)) (V2 170 30))
 
-drawText :: Text -> Maybe (Rectangle CInt) -> Resources -> SDL.Renderer -> IO ()
-drawText txt dest (Resources _ font) renderer = do
+drawText :: Text -> Maybe (Rectangle CInt) -> Resources -> Rendering ()
+drawText txt dest (Resources _ font) = do
     s <- SDL.Font.solid font (V4 0xFF 0xFF 0xFF 0xFF) txt
+    renderer <- ask
     texture <- SDL.createTextureFromSurface renderer s
     SDL.copy renderer texture Nothing dest
--- src/Hax.hs
+++ src/Hax.hs
@@ -6,12 +6,13 @@ where
 
 import Apecs (runWith)
 import Control.Monad (forM, unless)
+import Control.Monad.IO.Class (liftIO)
 import Data.Word (Word32)
 import Linear (V2(..))
 import qualified SDL
 import qualified SDL.Font
 
-import Drawing (draw)
+import Drawing (Rendering, runRendering, draw)
 import Game.Input (Input, initialInput, gatherInput)
 import Game.World
 import Resources (Resources, loadProjectResources)
@@ -39,7 +40,7 @@ run = do
     SDL.showWindow window
     -- Looping
     ticks <- SDL.ticks
-    mainLoop renderer ticks resources initialInput world
+    runRendering renderer $ mainLoop ticks resources initialInput world
     -- cleanup
     SDL.destroyWindow window
     SDL.Font.quit
@@ -47,18 +48,18 @@ run = do
 
 
 -- | The main loop of the game
-mainLoop :: SDL.Renderer -> Word32 -> Resources -> Input -> World -> IO ()
-mainLoop renderer ticks resources input world = do
+mainLoop :: Word32 -> Resources -> Input -> World -> Rendering ()
+mainLoop ticks resources input world = do
     events <- SDL.pollEvents
     toggleCheck <- SDL.getKeyboardState
     newTicks <- SDL.ticks
     let quit = any shouldQuit events
         newInput = gatherInput toggleCheck input
         dT = fromIntegral (newTicks - ticks) / 1000
-    toDraw <- runWith world (stepGame dT newInput)
-    draw toDraw resources renderer
+    toDraw <- liftIO $ runWith world (stepGame dT newInput)
+    draw toDraw resources
     unless quit $
-        mainLoop renderer newTicks resources newInput world
+        mainLoop newTicks resources newInput world
 
 
 -- | This is true of the window needs to close, or esc is prssed
